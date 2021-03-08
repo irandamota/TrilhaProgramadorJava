d4f7d593edc7b8eafd6891ec9c2bbe2ea9d1aff2
Iran da Mota <irandamota@gmail.com>
2021-03-07 23:15:55 -0300

    Primeiro commit com o código para imprimir uma mensagem.

---
+++ TrilhaProgramadorJava/src/Programa001.java
@@ -0,0 +1,12 @@
+/*
+Autor: Iran da Mota Andrade
+Literatura consultada: Como Programar Em Java 10ª - Deitel
+Atividade: Imprimir uma mensagem para o Usuário
+ */
+public class Programa001 {
+    public static void main(String[] args){
+
+        //Linha de comando para imprimir mensagem para o usuário
+        System.out.println("\nPrimeiro projeto em Java");
+    }
+}
